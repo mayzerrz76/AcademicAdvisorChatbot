081402a13ad7c310345aa9cc8d5501f73946e822
mayzerrz76 <79592272+mayzerrz76@users.noreply.github.com>
2022-02-27 21:50:02 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
+import json
 from flask import Flask, render_template, request
 from chatterbot import ChatBot
 from chatterbot.trainers import ChatterBotCorpusTrainer
+import spacy
 from pymongo import MongoClient
-import nltk
-import ssl
+
 
 app = Flask(__name__)
 
-english_bot = ChatBot("Chatterbot", storage_adapter="chatterbot.storage.SQLStorageAdapter")
+english_bot = ChatBot('Chatterbot')
 trainer = ChatterBotCorpusTrainer(english_bot)
 trainer.train("chatterbot.corpus.english")
 
@@ -21,11 +22,4 @@ def get_bot_response():
     return str(english_bot.get_response(userText))
 
 if __name__ == '__main__':
-    try:
-        _create_unverified_https_context = ssl._create_unverified_context
-    except AttributeError:
-        pass
-    else:
-        ssl._create_default_https_context = _create_unverified_https_context
-    nltk.download('punkt')
     app.run()
