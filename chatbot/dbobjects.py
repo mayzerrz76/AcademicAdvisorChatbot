e77834e53421665bdc99ce59f04b5738904145cc
Anthony Mazzacane <mazzacaneanthony@gmail.com>
2022-03-21 21:10:42 -0400

    Initial course class commit

--- chatbot/dbobjects.py
+++ chatbot/dbobjects.py
@@ -26,13 +26,13 @@ class UserAccount:
         self.planner = planner
         self.courses_registered = courses_registered
 
-        if courses_taken is None:
+        if self.courses_taken is None:
             self.courses_taken = []
-        if degree_programs is None:
+        if self.degree_programs is None:
             self.degree_programs = []
-        if planner is None:
+        if self.planner is None:
             self.planner = []
-        if courses_registered is None:
+        if self.courses_registered is None:
             self.courses_registered = []
 
         self.update_database()
@@ -43,15 +43,13 @@ class UserAccount:
 
     @staticmethod
     def from_mongo(document):
-        """
-
-        :param document: a string representing a username
-        :return:
-        """
         if type(document) is dict:
             return (lambda d: UserAccount(**d, from_mongo=True))(document)
         elif type(document) is str:
-            full_document = UserAccount.USERS.find({'username': document}).next()
+            try:
+                full_document = UserAccount.USERS.find({'username': document}).next()
+            except StopIteration:
+                raise KeyError('User does not exist')
             return UserAccount.from_mongo(full_document)
         else:
             raise TypeError('document must be a dict or a string')
@@ -63,3 +61,52 @@ class UserAccount:
     def is_valid_password(password):
         # TODO Add password requirement logic here
         return True
+
+
+class Course:
+    COURSES = DATABASE['courses']
+
+    def __init__(self, course_id: str, course_num: str, subject_code: str, section: str, location: str, instructor: str,
+                 days: str, times: str, description: str, prereqs=None, coreqs=None, from_mongo=False, _id=None):
+
+        # If this is a new database entry, validate the username
+        if not from_mongo:
+            identical_users = self.COURSES.find({'course_id': course_id})
+            if len(list(identical_users)):
+                raise KeyError('A user with that username already exists')
+
+        self.course_id = course_id
+        self.course_num = course_num
+        self.subject_code = subject_code
+        self.section = section
+        self.location = location
+        self.instructor = instructor
+        self.days = days
+        self.times = times
+        self.description = description
+        self.prereqs = prereqs
+        self.coreqs = coreqs
+
+        if self.prereqs is None:
+            self.prereqs = []
+        if self.coreqs is None:
+            self.coreqs = []
+
+        self.update_database()
+
+    def update_database(self):
+        Course.COURSES.delete_one({'course_id': self.course_id})
+        Course.COURSES.insert_one(self.__dict__)
+
+    @staticmethod
+    def from_mongo(document):
+        if type(document) is dict:
+            return (lambda d: Course(**d, from_mongo=True))(document)
+        elif type(document) is str:
+            try:
+                full_document = Course.COURSES.find({'course_id': document}).next()
+            except StopIteration:
+                raise KeyError('Course does not exist')
+            return Course.from_mongo(full_document)
+        else:
+            raise TypeError('document must be a dict or a string')
