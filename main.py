11118637d125305a515f85ce5739c3fc7c7f043c
Michael Perry <mperry03@syr.edu>
2022-03-23 17:16:51 -0400

    allows for beginnings of menu navigation!!!!

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def home():
     # allows for interacting with login page
@@ -11,13 +12,33 @@ def home():
     # allow for interacting with chatbot page
     return render_template("index.html")
 
-#NEED APP ROUTES FOR ALL SUB MENUS
+
+# NEED APP ROUTES FOR ALL SUB MENUS
+@app.route("/main")
+def navigate_main_menu():
+    select = request.args.get('num')
+    if select == "0":
+        return "logout"
+    elif select == "1":
+        return "list program reqs menu"
+    elif select == "2":
+        return "view course pre reqs menu"
+    elif select == "3":
+        return "build schedule menu"
+    elif select == "4":
+        return "view class description menu"
+    elif select == "5":
+        return "view my profile menu"
+    else:
+        return "please enter a whole number between 0 and 5"
+
 
 @app.route("/test")
 def echo_user_response():
     userText = request.args.get('msg')
     userDate = request.args.get('date')
     return str(userText) + " " + str(userDate)
+
 
 @app.route("/login")
 def validate_login():
@@ -31,7 +52,6 @@ def validate_login():
         return "correct login!"
     else:
         return "incorrect username and password"
-
 
 
 if __name__ == '__main__':
--- static/index.js
+++ static/index.js
@@ -5,32 +5,48 @@ script.type = 'text/javascript';
 document.getElementsByTagName('head')[0].appendChild(script);
 
 
-
+// allows for control flow to work
+var lol = true;
 // Creates the opening options for the chatbot!
-var opening = ["HOW CAN I HELP YOU?", "1) List Program Reqs.", "2) View Course Pre-Reqs.", "3) Build Schedule", "4) View Class Description", "5) View My Profile"];
-for (let i = 0; i < opening.length; i++ ) {
-    botSays(opening[i]);
+function makeOpening() {
+    var opening = ["HOW CAN I HELP YOU?", "1) List Program Reqs.", "2) View Course Pre-Reqs.", "3) Build Schedule", "4) View Class Description", "5) View My Profile"];
+    for (let i = 0; i < opening.length; i++ ) {
+        botSays(opening[i]);
+    }
 }
 
-// Function representing control flow to be added
-// function controlFlow() { waiting for keystrokes within nested if statements goes here }
+function controlFlow() {
+    var rawText = $("#textInput").val();
+    userSays(rawText);
+    $.get("/main", { num:rawText}, function(aiText){
+        botSays(aiText);
+        if (aiText != "please enter a whole number between 0 and 5") {
+            lol = false;
+        }
+        else {
+            makeOpening();
+        }
+    });
 
+}
 
 
 function getUserText() {
     // Retrieves the value from the #textinput html object
     var rawText = $("#textInput").val();
-    // Creates an object from the user input with bordering
-    var userHtml = '<p class="userText"><span>' + rawText + '</span></p>';
-    // Place user input into chatbox
-    $("#chatbox").append(userHtml);
-    // Remove previous entry from input box
-    $("#textInput").val("");
-
+    userSays(rawText);
     // finds the /test route, passes rawText as parameter msg, executes generic function with output as param
     $.get("/test", { msg:rawText, date:"3/22/22" }, function(aiText) {
             botSays(aiText);
     });
+}
+function userSays(str) {
+    // Creates an object from the user input with bordering
+    var userHtml = '<p class="userText"><span>' + str + '</span></p>';
+    // Place user input into chatbox
+    $("#chatbox").append(userHtml);
+    // Remove previous entry from input box
+    $("#textInput").val("");
 }
 
 function botSays(str) {
@@ -39,17 +55,16 @@ function botSays(str) {
         // Place ai output into chatbox
         $("#chatbox").append(botHtml);
 }
-
-function myFunction() {
-  const element = document.getElementById("content");
-  //element.scrollTop($(element))[0].scrollHeight);
-    element.scroll
-}
+makeOpening();
 
 // wait for enter key press
 $("#textInput").keypress(function(e) {
     if (e.which === 13) {
-        getUserText();
-        myFunction();
-}
+        if (lol == true){
+            controlFlow();
+        }
+        else
+            getUserText();
+
+    }
 });
